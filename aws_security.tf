adf7344bb1436686ca4271d7df6df10ba60e5d0c
Mélissa Otshudy <68287359+motshudy@users.noreply.github.com>
2021-05-24 12:35:43 +0100

    Update aws_security.tf

--- aws_security.tf
+++ aws_security.tf
@@ -8,6 +8,13 @@ ingress {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
+ingress {
+  description      = "http into VPC"
+  from_port        = 80
+  to_port          = 80
+  protocol         = "tcp"
+  cidr_blocks      = [0.0.0.0/24]
+}
 egress {
   from_port   = 0
   to_port     = 0
